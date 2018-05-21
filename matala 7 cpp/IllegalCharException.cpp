2c27020723a07d7a5e6c342baa7e91ac048b202c
IdosElmo <36295360+IdosElmo@users.noreply.github.com>
2018-05-21 20:10:52 +0300

    Delete IllegalCharException.cpp

--- matala 7 cpp/IllegalCharException.cpp
+++
@@ -1,9 +0,0 @@
-#include "IllegalCharException.h"
-
-IllegalCharException::IllegalCharException(char Char) {
-    this->Char = Char;
-}
-
-char IllegalCharException::TheChar() const {
-    return Char;
-}
